478aa8420f33e13df0e630e67cbc8d8662e155cb
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-31 06:33:07 -0700

    vue header

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,9 @@
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <span>{{ emoji }}</span>
+  </header>
 </template>
 
 <style scoped></style>

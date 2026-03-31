700a1b255d5de56904037194a0a793caa7ba7cb3
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-31 06:36:40 -0700

    vue main

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,11 @@
   <header>
     <span>{{ emoji }}</span>
   </header>
+
+  <main>
+    <h1>Hello, {{ name }}!</h1>
+    <p>I'am a Software Developer <a href="https://vuejs.org" target="_blank">Vue</a></p>
+  </main>
 </template>
 
 <style scoped></style>

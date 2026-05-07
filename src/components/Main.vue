1e93a418414faea21e5973586016a56142e95cd7
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-07 06:07:55 -0700

    changing main from app to main vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,7 +2,10 @@
 </script>
 
 <template>
-
+    <main>
+        <h1>Hello, {{name}}!</h1>
+        <p>I'm about to learn <a href="https://vuejs.org/" target="_blank">Maitashi</a>!</p>
+    </main>
 </template>
 
 <style scoped>
--- srcc/App.vue
+++ srcc/App.vue
@@ -9,10 +9,7 @@
 <template>
     
 
-<main>
-    <h1>Hello, {{name}}!</h1>
-    <p>I'm about to learn <a href="https://vuejs.org/" target="_blank">Maitashi</a>!</p>
-</main>
+
 
 <footer>
     <p>&copy; {{year}}</p>
